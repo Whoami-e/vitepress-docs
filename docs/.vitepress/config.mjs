d8a5fa3c0d6f9930cfc8af7f562d509d387c84f1
whoami <>
2024-08-05 20:29:41 +0800

    feat: 初始化仓库

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "../utils/auto-gen-sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "WHOAMI的文档站点",
+  description: "A VitePress Site",
+  head: [
+    ['link', { rel: 'icon', href: '/logo.svg' }]
+  ],
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
+    outline: {
+      label: '目录',
+      level: [2, 6]
+    },
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    footer: {
+      copyright: 'Copyright © 2024 WHOAMI'
+    },
+
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "WHOAMI的文档站点"
+  text: "构建知识库"
+  tagline: 持续学习，持续进步
+  image:
+    src: /background.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: 🍊 阅读
+    details: 书是人类进步的阶梯，书籍是知识的摇篮。
+  - title: 🍏 写作
+    details: 写作是最好的学习方式。
+  - title: 🍐 编码
+    details: 程序员的能力与他写过的代码是正比的。
+---
+
